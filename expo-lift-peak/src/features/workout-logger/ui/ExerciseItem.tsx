602268dd03c9e7386d64ca25adc22812f6fdfe6b
podnesTaF <podnestrade@gmail.com>
2024-08-08 21:26:52 +0100

    fix delete swipes

--- expo-lift-peak/src/features/workout-logger/ui/ExerciseItem.tsx
+++ expo-lift-peak/src/features/workout-logger/ui/ExerciseItem.tsx
@@ -1,6 +1,6 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import {IExerciseLog} from "@entities/workout-log";
-import {View, StyleSheet, Text, TouchableOpacity, Alert} from "react-native";
+import {View, StyleSheet, Text, Alert, Pressable} from "react-native";
 import {getExerciseTargetsToString} from "@features/workout-logger/utils";
 import {useExerciseStore} from "@features/workout-logger/store/exerciseStore";
 import {Colors} from "@shared/styles";
@@ -8,7 +8,7 @@ import Avatar from "@shared/components/Avatar";
 import {Ionicons} from "@expo/vector-icons";
 import * as Haptics from 'expo-haptics';
 import SwipeableRow from "@shared/components/SwipeableRow";
-import Animated, {FadeInUp, FadeOutUp, useAnimatedStyle, useSharedValue, withTiming} from "react-native-reanimated";
+import Animated, {FadeInUp, FadeOutUp} from "react-native-reanimated";
 
 interface ExerciseItemProps {
     exerciseLog: IExerciseLog;
@@ -18,6 +18,8 @@ interface ExerciseItemProps {
 
 export const ExerciseItem = ({exerciseLog, onPress, index}: ExerciseItemProps) => {
     const {getExerciseSetsStats} = useExerciseStore();
+    const [isPressed, setIsPressed] = useState(false);
+    const pressTimerRef = useRef<NodeJS.Timeout | null>(null);
 
 
 
@@ -33,41 +35,66 @@ export const ExerciseItem = ({exerciseLog, onPress, index}: ExerciseItemProps) =
         ])
     }
 
+    const handlePressIn = () => {
+        setIsPressed(true);
+        pressTimerRef.current = setTimeout(() => {
+            setIsPressed(false);
+        }, 200); // Adjust the timeout duration as needed
+    };
+
+    const handlePressOut = () => {
+        if (pressTimerRef.current) {
+            clearTimeout(pressTimerRef.current);
+        }
+        setIsPressed(false);
+    };
+
+    const handlePress = () => {
+        if (isPressed && onPress) {
+            onPress(exerciseLog.id);
+        }
+    };
+
 
     return (
         <SwipeableRow onDelete={confirmAlert}>
                 <Animated.View entering={FadeInUp.delay(index * 20)}
                                exiting={FadeOutUp}>
-                        <TouchableOpacity style={[styles.container]} onPress={() => onPress ? onPress(exerciseLog.id) : null}>
-                            <View style={{flexDirection: "row", alignItems: "center", gap: 16}}>
-                                <Avatar name={""} size={64} url={exerciseLog.exercise?.previewUrl}>
-                                    <Ionicons name={"barbell"} size={40} color={Colors.white}/>
-                                </Avatar>
-                                <View style={{justifyContent: "space-between", gap: 12}}>
-                                    <View>
-                                        <Text style={{
-                                            fontSize: 18,
-                                            textTransform: "uppercase",
-                                            fontWeight: "600",
-                                            color: "white"
-                                        }}>
-                                            {exerciseLog.exercise?.name}
-                                        </Text>
-                                        <Text style={{fontSize: 14, fontWeight: "500", color: Colors.dark300}}>
-                                            {getExerciseTargetsToString(exerciseLog.exercise?.exerciseTargets)}
-                                        </Text>
-                                    </View>
-                                    <View>
-                                        <Text style={{fontSize: 16, fontWeight: "700", color: Colors.lime}}>
-                                            {getExerciseSetsStats(exerciseLog.id).totalSets} / {getExerciseSetsStats(exerciseLog.id).setsDone} Sets
-                                            Done
-                                        </Text>
+                    <Pressable
+                        style={({pressed}) => [styles.container, {opacity: pressed ? 0.7 : 1}]}
+                        onPressIn={handlePressIn}
+                        onPressOut={handlePressOut}
+                        onPress={handlePress}
+                    >
+                                <View style={{flexDirection: "row", alignItems: "center", gap: 16}}>
+                                    <Avatar name={""} size={64} url={exerciseLog.exercise?.previewUrl}>
+                                        <Ionicons name={"barbell"} size={40} color={Colors.white}/>
+                                    </Avatar>
+                                    <View style={{justifyContent: "space-between", gap: 12}}>
+                                        <View>
+                                            <Text style={{
+                                                fontSize: 18,
+                                                textTransform: "uppercase",
+                                                fontWeight: "600",
+                                                color: "white"
+                                            }}>
+                                                {exerciseLog.exercise?.name}
+                                            </Text>
+                                            <Text style={{fontSize: 14, fontWeight: "500", color: Colors.dark300}}>
+                                                {getExerciseTargetsToString(exerciseLog.exercise?.exerciseTargets)}
+                                            </Text>
+                                        </View>
+                                        <View>
+                                            <Text style={{fontSize: 16, fontWeight: "700", color: Colors.lime}}>
+                                                {getExerciseSetsStats(exerciseLog.id).totalSets} / {getExerciseSetsStats(exerciseLog.id).setsDone} Sets
+                                                Done
+                                            </Text>
+                                        </View>
                                     </View>
                                 </View>
-                            </View>
 
-                            <Ionicons name={"chevron-forward"} size={32} color={Colors.lime}/>
-                        </TouchableOpacity>
+                                <Ionicons name={"chevron-forward"} size={32} color={Colors.lime}/>
+                            </Pressable>
                 </Animated.View>
         </SwipeableRow>
     );
