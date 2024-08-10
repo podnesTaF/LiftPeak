9faf595dae96df1ae3786ed524db8801126c8363
KostasV1l <r0926971@student.thomasmore.be>
2024-08-10 21:24:33 +0300

    before pulling

--- expo-lift-peak/src/shared/api/AxiosInstance.ts
+++ expo-lift-peak/src/shared/api/AxiosInstance.ts
@@ -2,7 +2,7 @@ import { useAuthStore } from "@features/auth";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.1.102:4000/api",
+  baseURL: process.env.API_BASE_URL,
 });
 
 api.interceptors.request.use(
