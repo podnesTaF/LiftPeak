9345a959f0ee5923360234c55cfb1ebb65d63b2e
KostasV1l <r0926971@student.thomasmore.be>
2024-09-21 16:06:50 +0300

    with conflictsv2

--- expo-lift-peak/src/shared/components/bottomSheet/CustomBottomSheet.tsx
+++ expo-lift-peak/src/shared/components/bottomSheet/CustomBottomSheet.tsx
@@ -12,7 +12,7 @@ import { renderBottomSheetBackdrop } from "./bottomSheetBackdrop";
 import { ViewStyle } from "react-native";
 
 interface CustomBottomSheetProps {
-
+<<<<<<< HEAD
   handleClose?: () => void;
   snapPoints?: (string | number)[];
   buttonName?: string;
@@ -22,7 +22,16 @@ interface CustomBottomSheetProps {
 
 const CustomBottomSheet = forwardRef<BottomSheetModal, CustomBottomSheetProps>(
   ({ handleClose, snapPoints, buttonName, buttonStyle, children }, ref) => {
+=======
+    handleClose: () => void;
+    snapPoints?: (string | number)[];
+    children?: React.ReactNode;
+    hideFooter?: boolean
+}
 
+const CustomBottomSheet = forwardRef<BottomSheetModal, CustomBottomSheetProps>(({handleClose, snapPoints,hideFooter,children}, ref) => {
+
+>>>>>>> b70f1c0987d6289c192ffe8fdb6a68c4600ef795
     const memoizedBackdrop = useCallback(renderBottomSheetBackdrop, []);
 
     const renderFooter = useCallback(
@@ -56,7 +65,7 @@ const CustomBottomSheet = forwardRef<BottomSheetModal, CustomBottomSheetProps>(
     
 
     return (
-
+<<<<<<< HEAD
       <BottomSheetModal
       footerComponent={handleClose ? renderFooter : undefined}
         backgroundStyle={{
@@ -75,7 +84,26 @@ const CustomBottomSheet = forwardRef<BottomSheetModal, CustomBottomSheetProps>(
           {children}
         </BottomSheetView>
       </BottomSheetModal>
-
+=======
+        <BottomSheetModal
+            footerComponent={!hideFooter ? renderFooter : undefined}
+            backgroundStyle={{
+                backgroundColor: Colors.dark700,
+            }}
+            backdropComponent={memoizedBackdrop}
+            ref={ref}
+            handleIndicatorStyle={{
+                backgroundColor: 'white',
+            }}
+            index={0}
+            enablePanDownToClose={true}
+            snapPoints={snapPoints || [ "80%"]}
+        >
+            <BottomSheetView style={{paddingVertical: 16, flex: 1}}>
+                {children}
+            </BottomSheetView>
+        </BottomSheetModal>
+>>>>>>> b70f1c0987d6289c192ffe8fdb6a68c4600ef795
     );
   }
 );
